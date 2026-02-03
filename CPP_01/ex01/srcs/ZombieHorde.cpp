8df4611d40bb9bf9f997f194f007097b4dd0916d
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-02-03 23:31:49 +0000

    CPP01 - ex01: Creating ZombieHorde function

--- CPP_01/ex01/srcs/ZombieHorde.cpp
+++ CPP_01/ex01/srcs/ZombieHorde.cpp
@@ -1,35 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*   ZombieHorde.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/15 16:25:27 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/02/03 23:29:52 by rmedeiro         ###   ########.fr       */
+/*   Created: 2026/01/15 23:26:32 by rmedeiro          #+#    #+#             */
+/*   Updated: 2026/02/03 23:29:18 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <iostream>
-# include <string>
+#include "../includes/Zombie.hpp"
 
-#ifndef ZOMBIE_HPP
-#define ZOMBIE_HPP
-
-#include <iostream>
-#include <string>
-
-class Zombie 
+Zombie* zombieHorde( int N, std::string name )
 {
-	private:
-		std::string _name;
-	public:
-        Zombie( void ); // * Default Constructor
-		~Zombie( void ); // * Destructor
-		void	announce( void );
-		void	setName(std::string name);
-};
+    Zombie *horde;
 
-Zombie*	zombieHorde( int N, std::string name );
-
-#endif
+    if (N <= 0)
+    {
+        std::cout << "The horde needs at least one Zombie!" << std::endl;
+        return (NULL);
+    }
+    horde = new Zombie[N];
+    for (int i = 0; i < N; i++)
+    {
+        horde[i].setName(name);
+    }
+    return (horde);
+}
