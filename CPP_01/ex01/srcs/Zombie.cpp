9b5a9989370e4f6255fedfce9e5d5cad82f3b598
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-02-04 16:07:11 +0000

    CPP01 - ex01: Done

--- CPP_01/ex01/srcs/Zombie.cpp
+++ CPP_01/ex01/srcs/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 16:23:32 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/02/03 23:42:22 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2026/02/04 16:03:01 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ Zombie::Zombie( void )
 
 Zombie::~Zombie( void )
 {
-    std::cout << this->_name << " has been destroyed" << std::endl;
+    std::cout << this->_name << " has been destroyed!" << std::endl;
 }
 
 void Zombie::announce( void )
