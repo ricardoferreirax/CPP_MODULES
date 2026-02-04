20ef7736ae7e78917774db4017559d5069444f6a
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-02-04 20:07:23 +0000

    CPP01 - ex03: Start creating the Weapon class. At time, created the default constructor and destructor

---
+++ CPP_01/ex03/includes/Weapon.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/16 16:34:28 by rmedeiro          #+#    #+#             */
+/*   Updated: 2026/02/04 20:04:45 by rmedeiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WEAPON_HPP
+#define WEAPON_HPP
+
+#include <iostream>
+#include <string.h>
+
+class Weapon
+{
+	private:
+		std::string _type;
+	public:
+		Weapon(std::string _type);
+		~Weapon(void);
+		Weapon(void);
+};
+
+#endif
---
+++ CPP_01/ex03/srcs/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/16 19:57:39 by rmedeiro          #+#    #+#             */
+/*   Updated: 2026/02/04 20:00:39 by rmedeiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/Weapon.hpp"
+
+int main()
+{
+    {
+        Weapon club = Weapon("crude spiked club");
+        /* HumanA bob("Bob", club);
+        bob.attack();
+        club.setType("some other type of club");
+        bob.attack(); */
+    }
+    {
+        Weapon club = Weapon("crude spiked club");
+        /* HumanB jim("Jim");
+        jim.setWeapon(club);
+        jim.attack();
+        club.setType("some other type of club");
+        jim.attack(); */
+    }
+    return (0);
+}
