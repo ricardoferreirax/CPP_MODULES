10adf365c42493f49b869fdf35320ef5627d646e
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-02-04 16:12:34 +0000

    CPP01 - ex01: Done

--- CPP_01/ex01/srcs/main.cpp
+++ CPP_01/ex01/srcs/main.cpp
@@ -6,29 +6,34 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 16:56:09 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/02/04 16:06:23 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2026/02/04 16:11:42 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Zombie.hpp"
 
-int main(void)
+int main(int ac, char **av)
 {
-	Zombie *horde;
-	int Nb;
+    Zombie *horde;
+    int Nb;
 
-	Nb = 10;
+    if (ac != 1)
+    {
+        std::cout << "Run the program only with: " << av[0] << std::endl;
+        return (1);
+    }
+    Nb = 10;
     std::cout << "Zombie Horde Creation: " << std::endl;
-	horde = zombieHorde(Nb, "Zombies");
-	std::cout << "\n";
-	std::cout << "Zombies Announce Themselves: " << std::endl;
-	for (int i = 0; i < Nb; i++)
-	{
-		std::cout << "[" << i << "] ";
-		horde[i].announce();
-	}
-	std::cout << "\n";
-	std::cout << "Zombie Horde Destruction: " << std::endl;
-	delete [] horde;
-	return (0);
+    horde = zombieHorde(Nb, "Zombies");
+    std::cout << "\n";
+    std::cout << "Zombies Announce Themselves: " << std::endl;
+    for (int i = 0; i < Nb; i++)
+    {
+        std::cout << "[" << i << "] ";
+        horde[i].announce();
+    }
+    std::cout << "\n";
+    std::cout << "Zombie Horde Destruction: " << std::endl;
+    delete [] horde;
+    return (0);
 }
