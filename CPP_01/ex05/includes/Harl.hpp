f3b81b494c7360a5479fdb725f660e7d96aefaaf
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-02-05 23:18:10 +0000

    CPP01 - ex05: Harl 2.0 started! Studying the best way to do this :)

---
+++ CPP_01/ex05/includes/Harl.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/05 23:12:47 by rmedeiro          #+#    #+#             */
+/*   Updated: 2026/02/05 23:14:38 by rmedeiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <iostream>
+#include <string>
+
+class Harl
+{
+	private:
+		void debug( void );
+		void info( void );
+		void warning( void );
+		void error( void );
+	public:
+		Harl( void );
+		~Harl( void );
+};
+
+#endif
