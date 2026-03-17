a24af40e070ce19ca82263f1a317ee443583177c
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-03-17 14:39:07 +0000

    Begin CPP_02: create include and srcs folder and start organizing the hpp file according to the orthodox canonical form

---
+++ CPP_02/ex00/includes/Fixed.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/17 11:47:22 by rmedeiro          #+#    #+#             */
+/*   Updated: 2026/03/17 14:36:47 by rmedeiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed
+{
+  private:
+	int _fixedPointNbr;
+	static const int _fractBits;
+
+  public:
+	Fixed(void);
+	Fixed(const Fixed &src);
+	Fixed &operator=(const Fixed &src);
+	~Fixed(void);
+
+};
+
+#endif
