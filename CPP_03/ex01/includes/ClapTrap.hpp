559bfc7d66a550c4ac435a936ce0e6c33545c9d6
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-03-23 23:23:59 +0000

    CPP03 - ex01: Organising folders and files

---
+++ CPP_03/ex01/includes/ClapTrap.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/20 15:58:52 by rmedeiro          #+#    #+#             */
+/*   Updated: 2026/03/23 23:19:41 by rmedeiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap
+{
+	private:
+		std::string	_name;
+		unsigned int			_hitPoints;
+		unsigned int			_energyPoints;
+		unsigned int			_attackDamage;
+	public:
+		ClapTrap(void);
+		ClapTrap(std::string name);
+		ClapTrap(const ClapTrap &src);
+		ClapTrap& operator=(const ClapTrap &src);
+		~ClapTrap(void);
+
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+};
+
+#endif
