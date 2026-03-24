c5cc04ca7d9dca47e399a8f6810b2ae426510fd8
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-03-24 14:56:34 +0000

    CPP03 - ex01: Done

--- CPP_03/ex01/srcs/main.cpp
+++ CPP_03/ex01/srcs/main.cpp
@@ -6,28 +6,12 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/23 23:21:48 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/03/24 14:50:17 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2026/03/24 14:55:50 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ClapTrap.hpp"
 #include "../includes/ScavTrap.hpp"
-
-/* int main(void)
-{
-	std::cout << "---- ClapTrap ----" << std::endl;
-	ClapTrap a("Ricky");
-	a.attack("Mercury");
-	a.takeDamage(5);
-	a.beRepaired(3);
-	std::cout << "\n---- ScavTrap ----" << std::endl;
-	ScavTrap b("Mercury");
-	b.attack("target");
-	b.takeDamage(30);
-	b.beRepaired(10);
-	b.guardGate();
-	return (0);
-} */
 
 int main(void)
 {
