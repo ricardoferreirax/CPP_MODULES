b949bc38864fd0d388184d2d227574d6a41a19b7
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-03-27 09:51:55 +0000

    CPP00 - ex00: Done

--- CPP_00/ex00/srcs/Megaphone.cpp
+++ CPP_00/ex00/srcs/Megaphone.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   Megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 06:22:51 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/01/14 14:51:14 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2026/03/27 09:50:06 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,25 @@
 
 int main(int ac, char **av)
 {
-    if (ac == 1)
+	int i;
+	int j;
+
+	if (ac == 1)
 	{
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-        return(1);
-    }
-    for (int i = 1; av[i]; i++)
-    {
-        for (int j = 0; av[i][j]; j++)
-            std::cout << (char)toupper(av[i][j]);
-    }
-    std::cout << std::endl;
-    return (0);
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (1);
+	}
+	i = 1;
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			std::cout << (char)toupper(av[i][j]);
+			j++;
+		}
+		i++;
+	}
+	std::cout << std::endl;
+	return (0);
 }
