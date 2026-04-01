4012e0e5a9c729b79e03d660ddad5182f89ea67a
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-04-01 10:40:45 +0100

    CPP00 - ex00: Done

--- CPP_00/ex00/srcs/Megaphone.cpp
+++ CPP_00/ex00/srcs/Megaphone.cpp
@@ -6,34 +6,29 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 06:22:51 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/03/27 09:50:06 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2026/04/01 10:35:00 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
+#include <cctype>
 
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
-	int i;
-	int j;
+    std::string str;
 
-	if (ac == 1)
-	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return (1);
-	}
-	i = 1;
-	while (i < ac)
-	{
-		j = 0;
-		while (av[i][j])
-		{
-			std::cout << (char)toupper(av[i][j]);
-			j++;
-		}
-		i++;
-	}
-	std::cout << std::endl;
-	return (0);
+    if (argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return (0);
+    }
+    for (int i = 1; i < argc; i++)
+    {
+        str = argv[i];
+        for (std::size_t j = 0; j < str.length(); j++)
+            std::cout << (char)std::toupper(str[j]);
+    }
+    std::cout << std::endl;
+    return (0);
 }
