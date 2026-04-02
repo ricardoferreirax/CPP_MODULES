3120567803e4cfddd7feb55e0105365ad3b346a0
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-04-02 15:04:34 +0100

    CPP01 - ex02: Done

--- CPP_01/ex02/srcs/main.cpp
+++ CPP_01/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/16 14:37:39 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/02/04 16:07:45 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2026/04/02 14:10:39 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,15 @@ int main(int ac, char **av)
         std::cout << "Usage: " << av[0] << std::endl;
         return (1);
     }
-    std::string     str = "HI THIS IS BRAIN";
-    std::string*    stringPTR = &str; // a pointer to the string
-    std::string&    stringREF = str;  // a reference to the string
+    std::string str = "HI THIS IS BRAIN";
+    std::string *stringPTR = &str; // a pointer to the string
+    std::string &stringREF = str;  // a reference to the string
 
-    std::cout << "--------- PRINT THE MEMORY ADDRESS OF VARIABLES ---------\n\n";
+    std::cout << "--------- MEMORY ADDRESSES ---------\n\n";
     std::cout << "The memory address of the string variable: " << &str << std::endl;
     std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
     std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
-    std::cout << "\n--------- PRINT THE VALUES OF VARIABLES ---------\n\n";
+    std::cout << "\n--------- VALUES ---------\n\n";
     std::cout << "The value of the string variable: " << str << std::endl;
     std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
     std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
