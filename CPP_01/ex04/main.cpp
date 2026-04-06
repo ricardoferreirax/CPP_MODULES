b2c83f1cdd23e0a0b1b863a92a21ef294442f172
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-04-06 22:14:10 +0100

    CPP01 - ex04: Done

---
+++ CPP_01/ex04/main.cpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/20 13:07:53 by rmedeiro          #+#    #+#             */
+/*   Updated: 2026/04/06 22:04:23 by rmedeiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+int	main(int ac, char **av)
+{
+	std::string	fileName;
+	std::string	toReplace;
+	std::string	replaceBy;
+	std::ifstream infile;
+	std::ofstream outfile;
+	std::string	 line;
+	std::size_t	found;
+
+	if (ac != 4)
+		return (std::cerr << "Usage: ./sed_is_for_losers <file> <s1> <s2>\n", 1);
+		
+	fileName = av[1];
+	toReplace = av[2];
+	replaceBy = av[3];
+
+	if (toReplace.empty())
+		return (std::cerr << "Error: s1 cannot be empty\n", 1);
+	infile.open(fileName.c_str());
+	if (!infile.is_open())
+		return (std::cerr << "Error: cannot open input file\n", 1);
+	outfile.open((fileName + ".replace").c_str());
+	if (!outfile.is_open())
+	{
+		infile.close();
+		return (std::cerr << "Error: cannot create output file\n", 1);
+	}
+	while (std::getline(infile, line))
+	{
+		found = line.find(toReplace);
+		while (found != std::string::npos)
+		{
+			line.erase(found, toReplace.length());
+			line.insert(found, replaceBy);
+			found = line.find(toReplace, found + replaceBy.length());
+		}
+		outfile << line;
+		if (!infile.eof())
+			outfile << "\n";
+	}
+	infile.close();
+	outfile.close();
+	return (0);
+}
