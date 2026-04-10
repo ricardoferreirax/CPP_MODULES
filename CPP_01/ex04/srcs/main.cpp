6a3731628bd805962a02c27c6795f7e05a4f485c
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-04-10 14:02:11 +0100

    CPP01 - ex04: Done

--- CPP_01/ex04/srcs/main.cpp
+++ CPP_01/ex04/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 13:07:53 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/04/06 22:22:03 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2026/04/10 14:01:04 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,32 +17,34 @@
 
 int	main(int ac, char **av)
 {
-	std::string	fileName;
-	std::string	toReplace;
-	std::string	replaceBy;
-	std::ifstream infile;
-	std::ofstream outfile;
+	std::string		fileName;
+	std::string		toReplace;
+	std::string		replaceBy;
+	std::ifstream	infile;
+	std::ofstream	outfile;
 
 	if (ac != 4)
-		return (std::cerr << "Usage: ./sed_is_for_losers <file> <s1> <s2>\n", 1);
-		
+		return (std::cerr << "Invalid Number of Arguments!\nUsage: ./Sed_is_for_losers <file> <s1> <s2>\n", 1);
+
 	fileName = av[1];
 	toReplace = av[2];
 	replaceBy = av[3];
-	
+
 	if (toReplace.empty())
 		return (std::cerr << "Error: s1 cannot be empty\n", 1);
+
 	infile.open(fileName.c_str());
 	if (!infile.is_open())
 		return (std::cerr << "Error: cannot open input file\n", 1);
 	infile.close();
+
 	outfile.open((fileName + ".replace").c_str());
 	if (!outfile.is_open())
 		return (std::cerr << "Error: cannot create output file\n", 1);
 	outfile.close();
-	
+
 	Sed sed(fileName, toReplace, replaceBy);
 	sed.processFile();
-	
+
 	return (0);
 }
