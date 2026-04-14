2433ccc6264797ddb16ee6db92d4bdab0c3738f0
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-04-14 13:56:06 +0100

    CPP01 - ex04: Done

--- CPP_01/ex04/srcs/Sed.cpp
+++ CPP_01/ex04/srcs/Sed.cpp
@@ -6,13 +6,15 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/06 22:17:52 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/04/06 22:21:46 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2026/04/14 13:54:53 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Sed.hpp"
 #include <fstream>
 
+// Constructor initializes the Sed object with the provided file name, the string 
+// to replace, and the string to replace it with.
 Sed::Sed(const std::string &fileName, const std::string &toReplace,
 	const std::string &replaceBy)
 {
@@ -21,6 +23,8 @@ Sed::Sed(const std::string &fileName, const std::string &toReplace,
 	this->_replaceBy = replaceBy;
 }
 
+// Takes a line of text and replaces all occurrences of the _toReplace string 
+// with the _replaceBy string. It returns the modified line.
 std::string	Sed::replaceLine(std::string line) const
 {
 	std::size_t	found;
@@ -35,6 +39,8 @@ std::string	Sed::replaceLine(std::string line) const
 	return (line);
 }
 
+// Reads the content of the file specified by _fileName, processes each line  using the replaceLine.
+// It writes the modified lines to a new file with the same name but with .replace.
 void	Sed::processFile(void) const
 {
 	std::ifstream infile(_fileName.c_str());
