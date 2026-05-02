f46c148320e09041588c3577955b599a11788616
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-05-02 12:19:46 +0100

    CPP04 - ex03: Done

--- CPP_04/ex03/srcs/main.cpp
+++ CPP_04/ex03/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/30 14:48:09 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/05/02 11:59:46 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2026/05/02 12:18:58 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,41 +20,91 @@
 
 int main(void)
 {
-	IMateriaSource *src = new MateriaSource();
-	std::cout << std::endl;
+	{
+		IMateriaSource *src = new MateriaSource();
+		std::cout << std::endl;
+		
+		src->learnMateria(new Ice());
+		std::cout << std::endl;
+		src->learnMateria(new Cure());
+		std::cout << std::endl;
+		ICharacter *me = new Character("me");
+		std::cout << std::endl;
+		ICharacter *bob = new Character("bob");
+		std::cout << std::endl;
 
-	src->learnMateria(new Ice());
-	std::cout << std::endl;
-	
-	src->learnMateria(new Cure());
-	std::cout << std::endl;
+		AMateria *tmp;
 
-	ICharacter *me = new Character("me");
-	std::cout << std::endl;
-	
-	AMateria *tmp;
+		tmp = src->createMateria("ice");
+		std::cout << std::endl;
+		me->equip(tmp);
+		std::cout << std::endl;
 
-	tmp = src->createMateria("ice");
-	std::cout << std::endl;
-	me->equip(tmp);
-	std::cout << std::endl;
-	
-	tmp = src->createMateria("cure");
-	std::cout << std::endl;
-	me->equip(tmp);
-	std::cout << std::endl;
-	
-	ICharacter *bob = new Character("bob");
-	std::cout << std::endl;
-	
-	me->use(0, *bob);
-	std::cout << std::endl;
-	me->use(1, *bob);
-	std::cout << std::endl;
-	
-	delete bob;
-	delete me;
-	delete src;
+		tmp = src->createMateria("cure");
+		std::cout << std::endl;
+		me->equip(tmp);
+		std::cout << std::endl;
 
+		me->use(0, *bob);
+		std::cout << std::endl;
+		me->use(1, *bob);
+		std::cout << std::endl;
+
+		delete bob;
+		delete me;
+		delete src;
+	}
+	std::cout << "---------------------------------------------------------\n" << std::endl;
+	{
+		IMateriaSource *src = new MateriaSource();
+		std::cout << std::endl;
+		
+		src->learnMateria(new Ice());
+		std::cout << std::endl;	
+		src->learnMateria(new Cure());
+		std::cout << std::endl;
+
+		ICharacter *me = new Character("me");
+		std::cout << std::endl;
+		ICharacter *bob = new Character("bob");
+		std::cout << std::endl;
+
+		AMateria *tmp;
+
+		tmp = src->createMateria("fire");
+		if (!tmp)
+			std::cout << "NULL OK" << std::endl;
+		std::cout << std::endl;
+
+		me->equip(NULL);
+		me->use(-1, *bob);
+		me->use(4, *bob);
+		me->unequip(-1);
+		me->unequip(4);
+		std::cout << std::endl;
+
+		tmp = src->createMateria("ice");
+		std::cout << std::endl;
+		me->equip(tmp);
+		me->unequip(0);
+		me->use(0, *bob);
+		me->unequip(0);
+		std::cout << std::endl;
+		
+		me->equip(src->createMateria("ice"));
+		std::cout << std::endl;
+		me->equip(src->createMateria("cure"));
+		std::cout << std::endl;
+		me->equip(src->createMateria("ice"));
+		std::cout << std::endl;
+		me->equip(src->createMateria("cure"));
+		std::cout << std::endl;
+		me->equip(src->createMateria("ice"));
+		std::cout << std::endl;
+
+		delete bob;
+		delete me;
+		delete src;
+	}
 	return (0);
 }
