b880c46b038363418bf18c497dbbacf76a25599e
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-05-02 11:51:13 +0100

    CPP04 - ex03: Done

--- CPP_04/ex03/srcs/main.cpp
+++ CPP_04/ex03/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/30 14:48:09 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/04/30 17:00:05 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2026/05/02 11:48:28 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,20 +21,36 @@
 int main(void)
 {
 	IMateriaSource *src = new MateriaSource();
+	std::cout << std::endl;
 
 	src->learnMateria(new Ice());
+	std::cout << std::endl;
+	
 	src->learnMateria(new Cure());
+	std::cout << std::endl;
 
 	ICharacter *me = new Character("me");
+	std::cout << std::endl;
+	
 	AMateria *tmp;
 
 	tmp = src->createMateria("ice");
+	std::cout << std::endl;
 	me->equip(tmp);
+	std::cout << std::endl;
+	
 	tmp = src->createMateria("cure");
+	std::cout << std::endl;
 	me->equip(tmp);
+	std::cout << std::endl;
+	
 	ICharacter *bob = new Character("bob");
+	std::cout << std::endl;
+	
 	me->use(0, *bob);
+	std::cout << std::endl;
 	me->use(1, *bob);
+	std::cout << std::endl;
 	
 	delete bob;
 	delete me;
