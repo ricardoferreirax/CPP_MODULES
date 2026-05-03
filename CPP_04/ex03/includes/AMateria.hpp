bca789b8dc1acd5c8c496872e4f324d285c60cb3
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-05-03 22:08:56 +0100

    CPP04 - ex03: Done

--- CPP_04/ex03/includes/AMateria.hpp
+++ CPP_04/ex03/includes/AMateria.hpp
@@ -6,7 +6,7 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/28 16:24:13 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/05/02 11:14:56 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2026/05/03 22:08:12 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,9 @@
 
 class ICharacter;
 
+// AMateria is abstract 'cause it contains at least one pure virtual function (clone). 
+// it serves as a base class for Ice and Cure types that will implement the clone method 
+// to allow copying of Materia objects through a common interface.
 class AMateria
 {
 	protected:
--- CPP_04/ex03/srcs/AMateria.cpp
+++ CPP_04/ex03/srcs/AMateria.cpp
@@ -6,7 +6,7 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/29 16:00:06 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/05/02 11:14:28 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2026/05/03 22:07:35 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,29 +18,37 @@ AMateria::AMateria(void)
 	std::cout << "AMateria default constructor called" << std::endl;
 }
 
+// we create an AMateria with a specific type, which will be used to identify the 
+// kind of Materia (ice, cure).
 AMateria::AMateria(std::string const &type)
 {
 	this->_type = type;
 	std::cout << "AMateria constructor called" << std::endl;
 }
 
+// we create a new AMateria by copying the type from an existing one.
+// this is used when clone() is called on a derived class (Ice or Cure) to create a new instance of the same type.
 AMateria::AMateria(const AMateria &src)
 {
 	this->_type = src._type;
 	std::cout << "AMateria copy constructor called" << std::endl;
 }
 
+// we don't need to copy anything for AMateria, because the type is set in the constructor and doesn't change.
 AMateria &AMateria::operator=(const AMateria &src)
 {
 	(void)src;
 	return (*this);
 }
 
+// the destructor is virtual to ensure that the correct destructor is called for derived classes (Ice, Cure) 
+// when an AMateria pointer is deleted.
 AMateria::~AMateria(void)
 {
 	std::cout << "AMateria destructor called" << std::endl;
 }
 
+// returns the type of the Materia, which is used to identify it (ice, cure).
 std::string const &AMateria::getType(void) const
 {
 	return (this->_type);
