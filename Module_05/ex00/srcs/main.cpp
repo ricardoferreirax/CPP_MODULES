63c55decb994b7a92acaac3ce6f9ecc05c56e8d0
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-05-28 22:41:21 +0100

    CPP05: Adjust Increment/decrement according to expection classes

--- Module_05/ex00/srcs/main.cpp
+++ Module_05/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/27 04:45:08 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/05/27 19:02:13 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2026/05/28 22:38:06 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,31 @@ int	main(void)
 {
 	try
 	{
-		Bureaucrat a("Ricky", 1);
+		Bureaucrat a("Ricky", 2);
 		std::cout << a.getName() << ", bureaucrat grade " << a.getGrade() << std::endl;
-
+		std::cout << std::endl;
+		
 		Bureaucrat b(a);
 		std::cout << b.getName() << ", bureaucrat grade " << b.getGrade() << std::endl;
-
+		std::cout << std::endl;
+		
 		Bureaucrat c("Default", 150);
-		std::cout << "Before assignment :" << c.getName() << ", bureaucrat grade " << c.getGrade() << std::endl;
+		std::cout << "Before assignment: " << c.getName() << ", bureaucrat grade " << c.getGrade() << std::endl;
 		c = a;
-		std::cout << "After assignment :" << c.getName() << ", bureaucrat grade " << c.getGrade() << std::endl;
-
+		std::cout << "After assignment: " << c.getName() << ", bureaucrat grade " << c.getGrade() << std::endl;
+		std::cout << std::endl;
+		
 		a.incrementGrade();
 		std::cout << a.getName() << ", bureaucrat grade " << a.getGrade() << std::endl;
+		std::cout << std::endl;
+		
+		c.decrementGrade();
+		std::cout << c.getName() << ", bureaucrat grade " << c.getGrade() << std::endl;
+		std::cout << std::endl;
 	}
 	catch (std::exception &e)
 	{
-		std::cerr << "Exception: " << e.what() << std::endl;
+		std::cerr << "\nException: " << e.what() << std::endl;
 	}
 	std::cout << std::endl;
 
