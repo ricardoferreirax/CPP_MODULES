8ca06a88d714907e108c953434f06a5f623eb1a9
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-08-08 20:45:49 +0100

    CPP06 - ex01: Done

--- Module_06/ex01/srcs/main.cpp
+++ Module_06/ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/08 18:10:45 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/08/08 20:30:34 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2026/08/08 20:44:54 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ int	main(void)
 	uintptr_t raw;
 
 	original = &data;
+	//original = new Data("Ricardo", 24);
 	res = NULL;
 
 	std::cout << "Original data: " << original->getName() << " | Age: " << original->getAge() << std::endl;
@@ -30,7 +31,7 @@ int	main(void)
 	std::cout << ">>>> Before deserialize <<<<<" << std::endl << std::endl;
 	std::cout << "Original pointer: " << original << std::endl;
 	std::cout << "Restored pointer: " << res << std::endl;
-	std::cout << "Are pointers equal? " << (original == res) << std::endl << std::endl;
+	std::cout << "Are pointers equal? " << (original == res ? "Yes" : "No") << std::endl;
 
 	raw = Serializer::serialize(original);
 	std::cout << "Serialized value: " << raw << std::endl << std::endl;
@@ -39,9 +40,11 @@ int	main(void)
 	std::cout << ">>>> After deserialize <<<<<" << std::endl << std::endl;
 	std::cout << "Original pointer: " << original << std::endl;
 	std::cout << "Restored pointer: " << res << std::endl;
-	std::cout << "Are pointers equal? " << (original == res) << std::endl << std::endl;
+	std::cout << "Are pointers equal? " << (original == res ? "Yes" : "No") << std::endl << std::endl;
 
 	std::cout << "Restored data: " << res->getName() << " | Age: " << res->getAge() << std::endl << std::endl;
+
+	//delete original;
 
 	return (0);
 }
