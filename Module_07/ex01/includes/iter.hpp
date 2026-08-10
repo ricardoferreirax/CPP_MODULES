3fbaf06c6a866799b2e295c6be147a89d1f863ae
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-08-10 18:31:19 +0100

    CPP07 - ex01: Done

--- Module_07/ex01/includes/iter.hpp
+++ Module_07/ex01/includes/iter.hpp
@@ -6,19 +6,56 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/10 17:19:11 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/08/10 17:22:52 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2026/08/10 18:24:47 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 # define ITER_HPP
 
-#include <cstddef>
+# include <iostream>
 
-template <typename T> void iter(T *array, const size_t length, void (*function)(T &))
+template <typename Data, typename Func> void iter(Data *array, const int len, Func func)
 {
-	for (size_t i = 0; i < length; i++)
-		function(array[i]);
+	int	i;
+
+	if (!array)
+		return ;
+	i = 0;
+	while (i < len)
+	{
+		func(array[i]);
+		i++;
+	}
+}
+
+template <typename Data, typename Func> void iter(const Data *array, const int len, Func func)
+{
+	int	i;
+
+	if (!array)
+		return ;
+	i = 0;
+	while (i < len)
+	{
+		func(array[i]);
+		i++;
+	}
+}
+
+template <typename Value> void printValue(const Value &value)
+{
+	std::cout << value << " ";
+}
+
+template <typename Value> void squareValue(Value &value)
+{
+	value *= value;
+}
+
+template <typename Value> void appendFortyTwo(Value &value)
+{
+	value += "_42";
 }
 
 #endif
--- Module_07/ex01/srcs/main.cpp
+++ Module_07/ex01/srcs/main.cpp
@@ -5,27 +5,44 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/08/10 17:20:08 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/08/10 17:21:51 by rmedeiro         ###   ########.fr       */
+/*   Created: 2026/08/10 18:15:52 by rmedeiro          #+#    #+#             */
+/*   Updated: 2026/08/10 18:29:40 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/iter.hpp"
-#include <iostream>
 
-void	increment(int &value)
-{
-	value++;
-}
+#include <iostream>
+#include <string>
 
 int	main(void)
 {
-	int	array[] = {1, 2, 3, 4, 5};
+	int	tab[] = {2, 3, 4, 5};
+	const int const_tab[] = {1, 2, 3, 4};
+	std::string	words[] = {"Project", "Module", ""};
 
-	iter(array, 5, increment);
+	std::cout << std::endl;
+	std::cout << "Integer array: ";
+	::iter(tab, 4, printValue<int>);
+	std::cout << std::endl;
 
-	for (int i = 0; i < 5; i++)
-		std::cout << array[i] << " " << std::endl;
+	std::cout << "Apply function: ";
+	::iter(tab, 4, squareValue<int>);
+	::iter(tab, 4, printValue<int>);
+	std::cout << std::endl << std::endl;
+
+	std::cout << "Const array: ";
+	::iter(const_tab, 4, printValue<int>);
+	std::cout << std::endl << std::endl;
+
+	std::cout << "String array:";
+	::iter(words, 3, printValue<std::string>);
+	std::cout << std::endl;
+
+	std::cout << "After append:  ";
+	::iter(words, 3, appendFortyTwo<std::string>);
+	::iter(words, 3, printValue<std::string>);
+	std::cout << std::endl << std::endl;
 
 	return (0);
 }
