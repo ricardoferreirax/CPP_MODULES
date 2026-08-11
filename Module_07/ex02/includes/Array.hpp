99a22df9202d686c8fb073918b8214ebe89ef789
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-08-11 17:21:38 +0100

    CPP07 - ex02: Inside the class template Array I implement directly the default and parametised constructors.

--- Module_07/ex02/includes/Array.hpp
+++ Module_07/ex02/includes/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/11 16:42:55 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/08/11 16:46:34 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2026/08/11 17:18:58 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define ARRAY_HPP
 
 # include <exception>
+# include <iostream>
 
 template <typename T> class Array
 {
@@ -22,12 +23,28 @@ template <typename T> class Array
 		unsigned int _length;
 
 	public:
-		Array(void);
-		Array(unsigned int n);
-		Array(const Array &other);
-		Array &operator=(const Array &other);
-		~Array(void);
+		Array(void)
+		{
+			std::cout << "[Array] Default constructor!" << std::endl;
+			this->_array = NULL;
+			this->_length = 0;
+		}
+
+		Array(unsigned int n)
+		{
+			std::cout << "[Array] Parameter constructor!" << std::endl;
+			this->_length = n;
+			if (this->_length > 0)
+				this->_array = new T[n]();
+			else
+				this->_array = NULL;
+		}
 		
+		~Array(void)
+		{
+			delete[] this->_array;
+			std::cout << "[Array] Destructor!" << std::endl;
+		}
 };
 
 #endif
--- Module_07/ex02/srcs/main.cpp
+++ Module_07/ex02/srcs/main.cpp
@@ -6,26 +6,18 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/11 16:47:27 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/08/11 16:53:34 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2026/08/11 17:19:59 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Array.hpp"
-#include "../includes/Array.tpp"
 
 #include <iostream>
 
 int	main(void)
 {
-	Array<int>	a;
-
-	Array<int>	b(5);
-
-	Array<int>	c(b);
-
-	Array<int>	d;
-
-	d = b;
+	Array<int> a;
+	Array<int> b(5);
 
 	std::cout << std::endl;
 
