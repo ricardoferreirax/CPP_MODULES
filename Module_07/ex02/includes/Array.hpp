7f91b65bbb3e2e3bfbad628f8f765c37d94c0bc7
Ricardo Medeiros <ricardomedeirosx@gmail.com>
2026-08-11 17:27:39 +0100

    CPP07 - ex02: Implement copy constructor

--- Module_07/ex02/includes/Array.hpp
+++ Module_07/ex02/includes/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/11 16:42:55 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/08/11 17:18:58 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2026/08/11 17:26:18 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,20 @@ template <typename T> class Array
 			this->_length = n;
 			if (this->_length > 0)
 				this->_array = new T[n]();
+			else
+				this->_array = NULL;
+		}
+
+		Array(const Array &other)
+		{			
+			std::cout << "[Array] Copy constructor!" << std::endl;
+			this->_length = other._length;
+			if (this->_length > 0)
+			{
+				this->_array = new T[this->_length];
+				for (unsigned int i = 0; i < this->_length; i++)
+					this->_array[i] = other._array[i];
+			}
 			else
 				this->_array = NULL;
 		}
--- Module_07/ex02/srcs/main.cpp
+++ Module_07/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/11 16:47:27 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/08/11 17:19:59 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2026/08/11 17:25:48 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int	main(void)
 {
 	Array<int> a;
 	Array<int> b(5);
+	Array<int> c(b);
 
 	std::cout << std::endl;
 
